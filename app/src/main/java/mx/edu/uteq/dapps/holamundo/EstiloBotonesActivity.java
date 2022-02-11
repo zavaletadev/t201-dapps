1a8ca3e41560b5f3f4f10abe0f159813e26ccbfe
Raúl <raul.zavaletazea@gmail.com>
2022-02-11 11:54:38 -0600

    Ejemplo de estilos para textview y material buttons implementando el estilo heredado de MAterialComponentes de MaterialDesign

---
+++ app/src/main/java/mx/edu/uteq/dapps/holamundo/EstiloBotonesActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.uteq.dapps.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstiloBotonesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estilo_botones);
+    }
+}
---
+++ app/src/main/java/mx/edu/uteq/dapps/holamundo/EstilosTextoActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.uteq.dapps.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstilosTextoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estilos_texto);
+    }
+}
