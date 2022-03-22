84581df695bb444100d658829a5e6b90a09e06d6
Raúl <raul.zavaletazea@gmail.com>
2022-03-21 20:25:32 -0600

    Navegación en Fragments

--- app/src/main/java/dev/zavaletazea/uteq/dapps/t201/proyectofinal/MainActivity.java
+++ app/src/main/java/dev/zavaletazea/uteq/dapps/t201/proyectofinal/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Evitamos regresar a esta pantalla
+        finish();
         startActivity(
                 new Intent(
                         MainActivity.this,
--- app/src/main/java/dev/zavaletazea/uteq/dapps/t201/proyectofinal/pub/PublicActivity.java
+++ app/src/main/java/dev/zavaletazea/uteq/dapps/t201/proyectofinal/pub/PublicActivity.java
@@ -1,9 +1,15 @@
 package dev.zavaletazea.uteq.dapps.t201.proyectofinal.pub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import dev.zavaletazea.uteq.dapps.t201.proyectofinal.R;
 import dev.zavaletazea.uteq.dapps.t201.proyectofinal.databinding.ActivityPublicBinding;
@@ -11,7 +17,8 @@ import dev.zavaletazea.uteq.dapps.t201.proyectofinal.databinding.ActivityPublicB
 public class PublicActivity extends AppCompatActivity {
 
     private ActivityPublicBinding binding;
-
+    private AppBarConfiguration appBarConfiguration;
+    private NavController navController;
 
 
     @Override
@@ -19,6 +26,50 @@ public class PublicActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPublicBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        /*
+        Creamos un contorlador del contenido
+        del fragmento
+         */
+        navController = Navigation.findNavController(
+                PublicActivity.this,
+                R.id.host_public_fragments
+        );
+
+        /*
+        Controlamos el actionbar desde las acciones
+        de los hijos del fragmento
+         */
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                navController.getGraph()
+        ).build();
+
+        /*
+        Indicamos quien controla el Actionbar
+         */
+        NavigationUI.setupActionBarWithNavController(
+                PublicActivity.this,
+                navController,
+                appBarConfiguration
+        );
+
+        /*
+        Click del FAB del carrito
+         */
+        binding.fabCarrito.setOnClickListener(view -> {
+            //Mostramos el fragment del carrito
+            navController.navigateUp();
+            navController.navigate(R.id.CarritoFragment);
+        });
+
+    } // oncreate
+
+    /*
+    Programamos el cambio del actionbar
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 
     /*
@@ -29,5 +80,29 @@ public class PublicActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_public, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    /*
+    Click de cada elemento del menu
+     */
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        /*
+        Dependiendo del ID del menu, indicamos que fragmento se muestra
+         */
+        switch(item.getItemId()) {
+            case R.id.menu_pub_catalogo:
+                //Mostramos el fragmento del catálogo
+                navController.navigateUp();
+                navController.navigate(R.id.CatalogoFragment);
+            break;
+
+            case R.id.menu_pub_login:
+                navController.navigateUp();
+                navController.navigate(R.id.LoginFragment);
+            break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
